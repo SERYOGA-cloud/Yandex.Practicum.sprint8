6b1703eff9525c1eaa192109fe4872b79b9056fc
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2025-10-27 19:21:25 +0500

    домашнее задание №3

--- app/src/main/java/com/msaggik/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/msaggik/playlistmaker/SettingActivity.kt
@@ -1,6 +1,9 @@
 package com.msaggik.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,13 @@ import androidx.core.view.WindowInsetsCompat
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_setting)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonBack = findViewById<ImageView>(R.id.button_back)
+
+        buttonBack.setOnClickListener {
+            val intent = Intent(this@SettingActivity, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
