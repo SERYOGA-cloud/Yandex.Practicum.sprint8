0e1c3c49ef55bcc23822d42e5a526ac56b6f68bd
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2025-10-27 23:41:54 +0500

    Revert "домашнее задание 1"
    This reverts commit b98595239226a3351a6cc2b3551f9d2593c060dc.

--- app/src/main/java/com/msaggik/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/msaggik/playlistmaker/MainActivity.kt
@@ -1,1 +1,45 @@
-package com.msaggik.playlistmakerimport android.content.Intentimport android.os.Bundleimport android.view.Viewimport android.widget.Buttonimport android.widget.Toastimport androidx.activity.enableEdgeToEdgeimport androidx.appcompat.app.AppCompatActivityimport androidx.core.view.ViewCompatimport androidx.core.view.WindowInsetsCompatclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_main)        val buttonSearch = findViewById<Button>(R.id.button_search)        val buttonMedia = findViewById<Button>(R.id.button_media)        val buttonSetting = findViewById<Button>(R.id.button_setting)        buttonSearch.setOnClickListener {            val intent = Intent(this@MainActivity, SearchActivity::class.java)            startActivity(intent)        }        buttonMedia.setOnClickListener(listener)        buttonSetting.setOnClickListener(listener)    }    private val listener: View.OnClickListener = object: View.OnClickListener {        override fun onClick(p0: View?) {            when(p0?.id) {                R.id.button_media -> {                    val intent = Intent(this@MainActivity, MediaActivity::class.java)                    startActivity(intent)                }                R.id.button_setting -> {                    val intent = Intent(this@MainActivity, SettingActivity::class.java)                    startActivity(intent)                }            }        }    }}
+package com.msaggik.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSetting = findViewById<Button>(R.id.button_setting)
+
+        buttonSearch.setOnClickListener {
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonMedia.setOnClickListener(listener)
+        buttonSetting.setOnClickListener(listener)
+    }
+
+    private val listener: View.OnClickListener = object: View.OnClickListener {
+        override fun onClick(p0: View?) {
+            when(p0?.id) {
+                R.id.button_media -> {
+                    val intent = Intent(this@MainActivity, MediaActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.button_setting -> {
+                    val intent = Intent(this@MainActivity, SettingActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
