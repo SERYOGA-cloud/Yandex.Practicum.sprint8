0bc77aa336952f27f82adb1f16490e3be9185a66
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2025-10-27 21:13:04 +0500

    домашнее задание 3 спринт 9

--- app/src/main/java/com/msaggik/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/msaggik/playlistmaker/SearchActivity.kt
@@ -1,6 +1,16 @@
 package com.msaggik.playlistmaker
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +20,49 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputSearch = findViewById<EditText>(R.id.input_search)
+        val buttonBack = findViewById<ImageView>(R.id.button_back)
+        val buttonClear = findViewById<Button>(R.id.button_clear)
+
+        val listener: View.OnClickListener = object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                when(p0?.id) {
+                    R.id.button_back -> {
+                        val backIntent = Intent(this@SearchActivity, MainActivity::class.java)
+                        startActivity(backIntent)
+                    }
+                    R.id.button_clear -> {
+                        inputSearch.setText("")
+                        inputSearch.isFocusable = false
+                        val keyboardOnOff = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                        keyboardOnOff?.hideSoftInputFromWindow(inputSearch.windowToken, 0)
+                    }
+                }
+            }
+        }
+
+        val inputSearchWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // просмотр старого текста
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // просмотр введённого текста
+                if(p0.isNullOrEmpty()) {
+                    buttonClear.visibility = View.GONE
+                } else {
+                    buttonClear.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // просмотр отредактированного текста
+            }
+        }
+
+        inputSearch.addTextChangedListener(inputSearchWatcher)
+        buttonBack.setOnClickListener(listener)
+        buttonClear.setOnClickListener(listener)
     }
 }
