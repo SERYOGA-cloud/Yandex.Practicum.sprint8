a9ecc7e28bb583161d4b67b854eaab45721579ff
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2025-10-29 15:10:47 +0500

    Правки (спринт №10)

--- app/src/main/java/com/msaggik/playlistmaker/util/TrackListAdapter.kt
+++ app/src/main/java/com/msaggik/playlistmaker/util/TrackListAdapter.kt
@@ -1,14 +1,13 @@
 package com.msaggik.playlistmaker.util
 
 import android.content.Context
-import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -31,22 +30,28 @@ class TrackListAdapter (private val trackList: List<Track>) : RecyclerView.Adapt
     class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         private val imageAlbumTrack: ImageView = itemView.findViewById(R.id.image_album_track)
-        private val nameTrack: TextView = itemView.findViewById(R.id.name_track)
+        private val trackName: TextView = itemView.findViewById(R.id.name_track)
         private val groupName: TextView = itemView.findViewById(R.id.group_name)
-        private val lengthTrack: TextView = itemView.findViewById(R.id.length_track)
+        private val trackLength: TextView = itemView.findViewById(R.id.length_track)
         private val buttonTrack: ImageView = itemView.findViewById(R.id.button_track)
 
         fun bind(model: Track) {
             Glide.with(itemView)
                 .load(model.artworkUrl100)
-                .placeholder(android.R.drawable.ic_menu_gallery)
+                .placeholder(R.drawable.ic_placeholder)
                 .centerCrop()
                 .transform(RoundedCorners(doToPx(2f, itemView.context.applicationContext)))
                 .into(imageAlbumTrack)
-            nameTrack.text = model.trackName
+            trackName.text = model.trackName
             groupName.text = model.artistName
-            lengthTrack.text = model.trackTime
-            buttonTrack.setOnClickListener(View.OnClickListener { Log.i("Выбор трека", model.trackName) })
+            trackLength.text = model.trackTime
+            buttonTrack.setOnClickListener(View.OnClickListener {
+                Toast.makeText(
+                    itemView.context.applicationContext,
+                    model.trackName,
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
         }
 
         private fun doToPx(dp: Float, context: Context): Int {
